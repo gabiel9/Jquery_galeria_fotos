459c6b507f877833bdbf5f3397c09b467015bfe6
Gabriel dos Santos <byeldossantos@gmail.com>
2024-01-17 02:20:57 -0300

    commit v2

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-    $('input').click(function ({
+    $('form').on('subumit', function (e) {
+        e.preventDefault();
 
+        const tarefaNova = $('#tarefa-nova').val();
+        const novoItem = $('<li> </li>');
+        $(`
+            <div>
+                <a href="${tarefaNova}" style="text-decoration: " />
+
+                </a>
+            </div>
         
-    }))
+        
+        
+        `)
+        
+    })
 
 
 
